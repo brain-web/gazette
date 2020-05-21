ad369fee467a0b0f33d7bfb8eff56df78b11cf5b
r03ert0 <rto@braincatalogue.org>
2020-05-21 11:05:42 +0200

    make svgs into pngs for email

--- prepare_email.sh
+++ prepare_email.sh
@@ -4,10 +4,11 @@ num=$1
 
 cd "$num"||return
 
-sed 's/src="img/src="https:\/\/brain-web.github.io\/gazette\/'"$num"'\/img/' index.html > tmp.html
-sed 's/src="..\/img/src="https:\/\/brain-web.github.io\/gazette\/img/' tmp.html > email.html
+cat index.html |
+sed 's/src="img/src="https:\/\/brain-web.github.io\/gazette\/'"$num"'\/img/' |
+sed 's/src="..\/img/src="https:\/\/brain-web.github.io\/gazette\/img/' |
+sed 's/.svg/.png/g' > email.html
 css-inliner-cli email.html
-rm tmp.html
 rm email.html
 mv email.css.html email.html
 
